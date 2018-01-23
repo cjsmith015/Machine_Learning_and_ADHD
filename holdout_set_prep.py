acb1527c84431f1849d7182777834027616b4cca
Christiana Smith <cj.smith015@gmail.com>
2018-01-22 18:01:16 -0800

    Remove .ipynb from gitignore, add current EDA notebook, for safekeeping.

--- holdout_set_prep.py
+++ holdout_set_prep.py
@@ -7,9 +7,9 @@ from sklearn.model_selection import train_test_split
 
 def write_csvs(df):
     """Write train and holdout data to csvs"""
-    train_data, holdout_data = train_test_split(df, test_size=0.20, random_state=56)
-    train_data.to_csv('data/train_data.csv')
-    holdout_data.to_csv('data/holdout_data.csv')
+    train_data, holdout_data = train_test_split(df.drop(columns='Unnamed: 0'), test_size=0.33, random_state=56)
+    train_data.to_csv('data/train_data.csv', index=False)
+    holdout_data.to_csv('data/holdout_data.csv', index=False)
 
 if __name__ == '__main__':
     full_data = pd.read_csv('data/Christie_diagnosis_20180118.csv')
