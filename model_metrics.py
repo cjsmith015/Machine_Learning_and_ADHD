4e8fd7549028d4ad3d4e8d3a21f6fb689847e7b7
Christiana Smith <cj.smith015@gmail.com>
2018-01-24 14:19:45 -0800

    Refactor model metrics to reflect improved metric loops

--- model_metrics.py
+++ model_metrics.py
@@ -1,6 +1,6 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-from sklearn.metrics import mean_squared_error, accuracy_score, auc, log_loss
+from sklearn.metrics import mean_squared_error, accuracy_score, roc_auc_score, log_loss
 from data_clean import *
 import pandas as pd
 import numpy as np
@@ -9,8 +9,8 @@ def run_classifiers(X_train, y_all,
                     list_of_classifiers = [LogisticRegression(), RandomForestClassifier(),
                     GradientBoostingClassifier()],
                     name_of_classifiers = ['LogReg', 'RandomForest', 'GradBoost'],
-                    list_of_metrics = [mean_squared_error, accuracy_score],
-                    name_of_metrics = ['mse', 'acc'],
+                    list_of_metrics = [accuracy_score, roc_auc_score, log_loss],
+                    name_of_metrics = ['acc', 'auc', 'logloss'],
                     n_folds=10):
     y_DX = y_all['DX']
     y_DXSUB = y_all['DXSUB']
@@ -18,24 +18,27 @@ def run_classifiers(X_train, y_all,
     list_of_data = [(X_train, y_DX), (X_train, y_DXSUB)]
     name_of_data = ['DX', 'DXSUB']
 
+    column_list = []
+    for data in name_of_data:
+        for metric in name_of_metrics:
+            column_list.append(data + '_' + metric + '_train')
+            column_list.append(data + '_' + metric + '_test')
+
     metrics_df = pd.DataFrame(data=None,
-                              columns=['DX_acc_train', 'DX_acc_test', 'DX_mse_train', 'DX_mse_test',
-                                       'DXSUB_acc_train', 'DXSUB_acc_test', 'DXSUB_mse_train', 'DXSUB_mse_test'],
-                              index=['LogReg', 'RandomForest', 'GradBoost'])
+                              columns=column_list,
+                              index=name_of_classifiers)
 
     for (X, y), data_name in zip(list_of_data, name_of_data):
-        for metric, metric_name in zip(list_of_metrics, name_of_metrics):
-            for clf, clf_name in zip(list_of_classifiers, name_of_classifiers):
-                train_metric, test_metric = cv(X.values, y.values,
-                                               clf, n_folds=10,
-                                               metric=metric)
+        for clf, clf_name in zip(list_of_classifiers, name_of_classifiers):
+            train_cv_metric, test_cv_metric = cv(X.values, y.values,
+                                           clf, n_folds=n_folds,
+                                           metrics=list_of_metrics)
+            train_metric = np.mean(train_cv_metric, axis=0)
+            test_metric = np.mean(test_cv_metric, axis=0)
+            for idx, metric in enumerate(name_of_metrics):
+                col_train = data_name + '_' + metric + '_train'
+                col_test = data_name + '_' + metric + '_test'
+                metrics_df[col_train].loc[clf_name] = train_metric[idx]
+                metrics_df[col_test].loc[clf_name] = test_metric[idx]
 
-                col_train_name = data_name + '_' + metric_name + '_train'
-                col_test_name = data_name + '_' + metric_name + '_test'
-
-                metrics_df[col_train_name].loc[clf_name] = np.mean(train_metric)
-                metrics_df[col_test_name].loc[clf_name] = np.mean(test_metric)
-
-    dx_df = metrics_df[['DX_acc_train', 'DX_acc_test', 'DX_mse_train', 'DX_mse_test']]
-    dxsub_df = metrics_df[['DXSUB_acc_train', 'DXSUB_acc_test', 'DXSUB_mse_train', 'DXSUB_mse_test']]
-    return dx_df, dxsub_df
+    return metrics_df
