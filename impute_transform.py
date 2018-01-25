a60996c4575ac92fa0b9d93bc50e5cf6fd6b858a
Christiana Smith <cj.smith015@gmail.com>
2018-01-24 17:31:08 -0800

    Final thoughts for the day

---
+++ impute_transform.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+from fancyimpute import MICE, MatrixFactorization
+
+class ImputeTransform(object):
+    def __init__(self, strategy=MICE()):
+        self.solver = strategy
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        if np.sum(np.isnan(X.values)) > 0:
+            return self.solver.complete(X)
+        else:
+            return X
