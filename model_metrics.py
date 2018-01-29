2a50644034244449836102efce72a955c44be770
Christiana Smith <cj.smith015@gmail.com>
2018-01-29 09:20:41 -0800

    Add multiclass functionality write function

--- model_metrics.py
+++ model_metrics.py
@@ -8,10 +8,11 @@ from sklearn.metrics import roc_auc_score
 from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.metrics.scorer import make_scorer
 
 from xgboost import XGBClassifier
 
-from fancyimpute import MatrixFactorization
+from fancyimpute import MatrixFactorization, SimpleFill
 
 from impute_transform import ImputeTransform
 
@@ -55,7 +56,13 @@ def multiclass_roc_auc_score(truth, pred, average=None):
     truth = lb.transform(truth)
     pred = lb.transform(pred)
 
-    return list(roc_auc_score(truth, pred, average=average))
+    with open('multiclass.txt', 'a') as f:
+        data = list(roc_auc_score(truth, pred, average=None))
+        data.append(truth.shape[0])
+        f.write(str(data))
+        f.write('\n')
+
+    return roc_auc_score(truth, pred, average='macro')
 
 def _run_clfs(clf_list, clf_names,
                 X, y, scoring,
@@ -92,55 +99,75 @@ if __name__ == '__main__':
     csv_name = sys.argv[1]
 
     train_data = pd.read_csv('data/train_data.csv')
-    #train_data_small = train_data.sample(n=200)
-    X = train_data.drop(columns=['DX','DXSUB'])
-    y = train_data['DX'].map({3:1, 1:0})
+    train_data_small = train_data.sample(n=400)
+    X = train_data_small.drop(columns=['DX','DXSUB'])
+    y = train_data_small['DX'].map({3:1, 1:0})
+    ysub = train_data_small['DXSUB']
 
    # X_TMCQ = train_data[['Y1_P_TMCQ_ACTIVCONT', 'Y1_P_TMCQ_ACTIVITY', 'Y1_P_TMCQ_AFFIL',
    #    'Y1_P_TMCQ_ANGER', 'Y1_P_TMCQ_FEAR', 'Y1_P_TMCQ_HIP',
-   #    'Y1_P_TMCQ_IMPULS', 'Y1_P_TMCQ_INHIBIT', 'Y1_P_TMCQ_SAD',
-   #    'Y1_P_TMCQ_SHY', 'Y1_P_TMCQ_SOOTHE', 'Y1_P_TMCQ_ASSERT',
-   #    'Y1_P_TMCQ_ATTFOCUS', 'Y1_P_TMCQ_LIP', 'Y1_P_TMCQ_PERCEPT',
-   #    'Y1_P_TMCQ_DISCOMF', 'Y1_P_TMCQ_OPENNESS', 'Y1_P_TMCQ_SURGENCY',
-   #    'Y1_P_TMCQ_EFFCONT', 'Y1_P_TMCQ_NEGAFFECT']]
-
-    #X_TMCQ_nonull = X_TMCQ[X_TMCQ.isnull().sum(axis=1) == 0]
-    #y_TMCQ_nonull = y[X_TMCQ.isnull().sum(axis=1) == 0]
-
-    X_neuro = train_data[['STOP_SSRTAVE_Y1', 'DPRIME1_Y1', 'DPRIME2_Y1', 'SSBK_NUMCOMPLETE_Y1',
-        'SSFD_NUMCOMPLETE_Y1', 'V_Y1', 'Y1_CLWRD_COND1', 'Y1_CLWRD_COND2',
-        'Y1_DIGITS_BKWD_RS', 'Y1_DIGITS_FRWD_RS', 'Y1_TRAILS_COND2',
-        'Y1_TRAILS_COND3', 'CW_RES', 'TR_RES', 'Y1_TAP_SD_TOT_CLOCK']]
-    X_neuro_nonull = X_neuro[X_neuro.isnull().sum(axis=1) != X_neuro.shape[1]]
-    y_neuro_nonull = y[X_neuro.isnull().sum(axis=1) != X_neuro.shape[1]]
+   #     'Y1_P_TMCQ_IMPULS', 'Y1_P_TMCQ_INHIBIT', 'Y1_P_TMCQ_SAD',
+   #     'Y1_P_TMCQ_SHY', 'Y1_P_TMCQ_SOOTHE', 'Y1_P_TMCQ_ASSERT',
+   #     'Y1_P_TMCQ_ATTFOCUS', 'Y1_P_TMCQ_LIP', 'Y1_P_TMCQ_PERCEPT',
+   #     'Y1_P_TMCQ_DISCOMF', 'Y1_P_TMCQ_OPENNESS', 'Y1_P_TMCQ_SURGENCY',
+   #     'Y1_P_TMCQ_EFFCONT', 'Y1_P_TMCQ_NEGAFFECT']]
+   #
+   #  X_TMCQ_nonull = X_TMCQ[X_TMCQ.isnull().sum(axis=1) == 0]
+   #  y_TMCQ_nonull = y[X_TMCQ.isnull().sum(axis=1) == 0]
+   #
+   #  X_neuro = train_data[['STOP_SSRTAVE_Y1', 'DPRIME1_Y1', 'DPRIME2_Y1', 'SSBK_NUMCOMPLETE_Y1',
+   #      'SSFD_NUMCOMPLETE_Y1', 'V_Y1', 'Y1_CLWRD_COND1', 'Y1_CLWRD_COND2',
+   #      'Y1_DIGITS_BKWD_RS', 'Y1_DIGITS_FRWD_RS', 'Y1_TRAILS_COND2',
+   #      'Y1_TRAILS_COND3', 'CW_RES', 'TR_RES', 'Y1_TAP_SD_TOT_CLOCK']]
+   #  X_neuro_nonull = X_neuro[X_neuro.isnull().sum(axis=1) != X_neuro.shape[1]]
+   #  y_neuro_nonull = y[X_neuro.isnull().sum(axis=1) != X_neuro.shape[1]]
 
     # make pipeline
-    log_reg_clf = make_pipeline(ImputeTransform(strategy=MatrixFactorization()),
+    log_reg_clf = make_pipeline(ImputeTransform(strategy=SimpleFill()),
                         LogisticRegression(random_state=56))
-    
-    rf_clf = make_pipeline(ImputeTransform(strategy=MatrixFactorization()),
+
+    rf_clf = make_pipeline(ImputeTransform(strategy=SimpleFill()),
                            RandomForestClassifier(n_jobs=-1, random_state=56))
-    
-    gb_clf = make_pipeline(ImputeTransform(strategy=MatrixFactorization()),
+
+    gb_clf = make_pipeline(ImputeTransform(strategy=SimpleFill()),
                            GradientBoostingClassifier(random_state=56))
-    
-    xgb_clf = make_pipeline(ImputeTransform(strategy=MatrixFactorization()),
+
+    xgb_clf = make_pipeline(ImputeTransform(strategy=SimpleFill()),
                             XGBClassifier(max_depth=3, learning_rate=0.1,
                             random_state=56, n_jobs=-1))
+    # log_reg_clf = make_pipeline(ImputeTransform(strategy=MatrixFactorization()),
+    #                     LogisticRegression(random_state=56))
+    #
+    # rf_clf = make_pipeline(ImputeTransform(strategy=MatrixFactorization()),
+    #                        RandomForestClassifier(n_jobs=-1, random_state=56))
+    #
+    # gb_clf = make_pipeline(ImputeTransform(strategy=MatrixFactorization()),
+    #                        GradientBoostingClassifier(random_state=56))
+    #
+    # xgb_clf = make_pipeline(ImputeTransform(strategy=MatrixFactorization()),
+    #                         XGBClassifier(max_depth=3, learning_rate=0.1,
+    #                         random_state=56, n_jobs=-1))
 
-    # Don't need imputation for TMCQ because I removed NaNs
-    #log_reg_clf = LogisticRegression(random_state=56)
-#
- #   rf_clf = RandomForestClassifier(n_jobs=-1, random_state=56)
-#
- #   gb_clf = GradientBoostingClassifier(random_state=56)
-#
- #   xgb_clf = XGBClassifier(max_depth=3, learning_rate=0.1,
-  ##                      random_state=56)
+    # #Don't need imputation for TMCQ because I removed NaNs
+    # log_reg_clf = LogisticRegression(random_state=56)
+    #
+    # rf_clf = RandomForestClassifier(n_jobs=-1, random_state=56)
+    #
+    # gb_clf = GradientBoostingClassifier(random_state=56)
+    #
+    # xgb_clf = XGBClassifier(max_depth=3, learning_rate=0.1,
+    #                 random_state=56)
 
+    # for multiclass
+    multiclass_roc = make_scorer(multiclass_roc_auc_score,
+                             greater_is_better=True)
+
+    scoring_dict = {'accuracy': 'accuracy',
+                'neg_log_loss': 'neg_log_loss',
+                'roc_auc': multiclass_roc}
 
     # create lists
-    scoring_list = ['accuracy', 'roc_auc', 'neg_log_loss']
+    #scoring_list = ['accuracy', 'roc_auc', 'neg_log_loss']
 
     classifier_list = [log_reg_clf, rf_clf, gb_clf, xgb_clf]
     classifier_name = ['LogReg', 'RandomForest', 'GradientBoosting', 'XGB']
@@ -148,10 +175,9 @@ if __name__ == '__main__':
     metrics_of_interest = ['fit_time', 'score_time', 'test_accuracy',
                        'test_neg_log_loss', 'test_roc_auc']
 
-    metric_dx_df = get_metrics(X_neuro_nonull, y_neuro_nonull,
+    metric_dx_df = get_metrics(X, ysub,
                                 classifier_list, classifier_name,
-                                scoring_list, metrics_of_interest,
+                                scoring_dict, metrics_of_interest,
                                 n_folds=5)
-    
-    metric_dx_df.to_csv(csv_name)
 
+    metric_dx_df.to_csv(csv_name)
