17c637f00273ab6a40c0fbcdb73a15d89e6c8c67
Christiana Smith <cj.smith015@gmail.com>
2018-01-29 10:41:52 -0800

    Refactor model metrics code

--- model_metrics.py
+++ model_metrics.py
@@ -95,77 +95,98 @@ def _fill_df(df, clf_dict, clf_names, cols):
             df[col].loc[clf] = clf_dict[clf][col]
     return df
 
+def prep_x_y(df, dataset_type):
+    if dataset_type == 'all_dx':
+        X = df.drop(columns=['DX','DXSUB'])
+        y = df['DX'].map({3:1, 1:0})
+        return X, y
+    elif dataset_type == 'all_dxsub'
+        X = df.drop(columns=['DX','DXSUB'])
+        y = df['DXSUB']
+        return X, y
+    elif dataset_type == 'tmcq':
+        X_TMCQ = df[['Y1_P_TMCQ_ACTIVCONT', 'Y1_P_TMCQ_ACTIVITY', 'Y1_P_TMCQ_AFFIL',
+          'Y1_P_TMCQ_ANGER', 'Y1_P_TMCQ_FEAR', 'Y1_P_TMCQ_HIP',
+           'Y1_P_TMCQ_IMPULS', 'Y1_P_TMCQ_INHIBIT', 'Y1_P_TMCQ_SAD',
+           'Y1_P_TMCQ_SHY', 'Y1_P_TMCQ_SOOTHE', 'Y1_P_TMCQ_ASSERT',
+           'Y1_P_TMCQ_ATTFOCUS', 'Y1_P_TMCQ_LIP', 'Y1_P_TMCQ_PERCEPT',
+           'Y1_P_TMCQ_DISCOMF', 'Y1_P_TMCQ_OPENNESS', 'Y1_P_TMCQ_SURGENCY',
+           'Y1_P_TMCQ_EFFCONT', 'Y1_P_TMCQ_NEGAFFECT']]
+        y = df['DX'].map({3:1, 1:0})
+
+        X_TMCQ_nonull = X_TMCQ[X_TMCQ.isnull().sum(axis=1) == 0]
+        y_TMCQ_nonull = y[X_TMCQ.isnull().sum(axis=1) == 0]
+        return X_TMCQ_nonull, y_TMCQ_nonull
+    elif dataset_type == 'neuro':
+        X_neuro = df[['STOP_SSRTAVE_Y1', 'DPRIME1_Y1', 'DPRIME2_Y1', 'SSBK_NUMCOMPLETE_Y1',
+            'SSFD_NUMCOMPLETE_Y1', 'V_Y1', 'Y1_CLWRD_COND1', 'Y1_CLWRD_COND2',
+            'Y1_DIGITS_BKWD_RS', 'Y1_DIGITS_FRWD_RS', 'Y1_TRAILS_COND2',
+            'Y1_TRAILS_COND3', 'CW_RES', 'TR_RES', 'Y1_TAP_SD_TOT_CLOCK']]
+        y = df['DX'].map({3:1, 1:0})
+
+        X_neuro_nonull = X_neuro[X_neuro.isnull().sum(axis=1) != X_neuro.shape[1]]
+        y_neuro_nonull = y[X_neuro.isnull().sum(axis=1) != X_neuro.shape[1]]
+        return X_neuro_nonull, y_neuro_nonull
+
+def prep_clfs(dataset_type):
+    if dataset_type == 'tmcq':
+        log_reg_clf = LogisticRegression(random_state=56)
+
+        rf_clf = RandomForestClassifier(n_jobs=-1, random_state=56)
+
+        gb_clf = GradientBoostingClassifier(random_state=56)
+
+        xgb_clf = XGBClassifier(max_depth=3, learning_rate=0.1,
+                        random_state=56)
+    else:
+        log_reg_clf = make_pipeline(ImputeTransform(strategy=MatrixFactorization()),
+                            LogisticRegression(random_state=56))
+
+        rf_clf = make_pipeline(ImputeTransform(strategy=MatrixFactorization()),
+                               RandomForestClassifier(n_jobs=-1, random_state=56))
+
+        gb_clf = make_pipeline(ImputeTransform(strategy=MatrixFactorization()),
+                               GradientBoostingClassifier(random_state=56))
+
+        xgb_clf = make_pipeline(ImputeTransform(strategy=MatrixFactorization()),
+                                XGBClassifier(max_depth=3, learning_rate=0.1,
+                                random_state=56, n_jobs=-1))
+    classifier_list = [log_reg_clf, rf_clf, gb_clf, xgb_clf]
+    return classifier_list
+
+def prep_scoring(dataset_type):
+    if dataset_type == 'all_dxsub':
+        multiclass_roc = make_scorer(multiclass_roc_auc_score,
+                                 greater_is_better=True)
+
+        scoring_dict = {'accuracy': 'accuracy',
+                    'neg_log_loss': 'neg_log_loss',
+                    'roc_auc': multiclass_roc}
+        return scoring_dict
+    else:
+        return ['accuracy', 'roc_auc', 'neg_log_loss']
+
 if __name__ == '__main__':
     csv_name = sys.argv[1]
+    dataset = sys.argv[2]
 
     train_data = pd.read_csv('data/train_data.csv')
-    #train_data_small = train_data.sample(n=400)
-    X = train_data.drop(columns=['DX','DXSUB'])
-    y = train_data['DX'].map({3:1, 1:0})
-    ysub = train_data['DXSUB']
 
-   # X_TMCQ = train_data[['Y1_P_TMCQ_ACTIVCONT', 'Y1_P_TMCQ_ACTIVITY', 'Y1_P_TMCQ_AFFIL',
-   #    'Y1_P_TMCQ_ANGER', 'Y1_P_TMCQ_FEAR', 'Y1_P_TMCQ_HIP',
-   #     'Y1_P_TMCQ_IMPULS', 'Y1_P_TMCQ_INHIBIT', 'Y1_P_TMCQ_SAD',
-   #     'Y1_P_TMCQ_SHY', 'Y1_P_TMCQ_SOOTHE', 'Y1_P_TMCQ_ASSERT',
-   #     'Y1_P_TMCQ_ATTFOCUS', 'Y1_P_TMCQ_LIP', 'Y1_P_TMCQ_PERCEPT',
-   #     'Y1_P_TMCQ_DISCOMF', 'Y1_P_TMCQ_OPENNESS', 'Y1_P_TMCQ_SURGENCY',
-   #     'Y1_P_TMCQ_EFFCONT', 'Y1_P_TMCQ_NEGAFFECT']]
-   #
-   #  X_TMCQ_nonull = X_TMCQ[X_TMCQ.isnull().sum(axis=1) == 0]
-   #  y_TMCQ_nonull = y[X_TMCQ.isnull().sum(axis=1) == 0]
-   #
-   #  X_neuro = train_data[['STOP_SSRTAVE_Y1', 'DPRIME1_Y1', 'DPRIME2_Y1', 'SSBK_NUMCOMPLETE_Y1',
-   #      'SSFD_NUMCOMPLETE_Y1', 'V_Y1', 'Y1_CLWRD_COND1', 'Y1_CLWRD_COND2',
-   #      'Y1_DIGITS_BKWD_RS', 'Y1_DIGITS_FRWD_RS', 'Y1_TRAILS_COND2',
-   #      'Y1_TRAILS_COND3', 'CW_RES', 'TR_RES', 'Y1_TAP_SD_TOT_CLOCK']]
-   #  X_neuro_nonull = X_neuro[X_neuro.isnull().sum(axis=1) != X_neuro.shape[1]]
-   #  y_neuro_nonull = y[X_neuro.isnull().sum(axis=1) != X_neuro.shape[1]]
+    # Prep stuff
+    X, y = prep_x_y(train_data, dataset)
+    classifier_list = prep_clfs(dataset)
+    scoring = prep_scoring(dataset)
 
-    # make pipeline
-    log_reg_clf = make_pipeline(ImputeTransform(strategy=MatrixFactorization()),
-                        LogisticRegression(random_state=56))
-
-    rf_clf = make_pipeline(ImputeTransform(strategy=MatrixFactorization()),
-                           RandomForestClassifier(n_jobs=-1, random_state=56))
-
-    gb_clf = make_pipeline(ImputeTransform(strategy=MatrixFactorization()),
-                           GradientBoostingClassifier(random_state=56))
-
-    xgb_clf = make_pipeline(ImputeTransform(strategy=MatrixFactorization()),
-                            XGBClassifier(max_depth=3, learning_rate=0.1,
-                            random_state=56, n_jobs=-1))
-
-    # #Don't need imputation for TMCQ because I removed NaNs
-    # log_reg_clf = LogisticRegression(random_state=56)
-    #
-    # rf_clf = RandomForestClassifier(n_jobs=-1, random_state=56)
-    #
-    # gb_clf = GradientBoostingClassifier(random_state=56)
-    #
-    # xgb_clf = XGBClassifier(max_depth=3, learning_rate=0.1,
-    #                 random_state=56)
-
-    # for multiclass
-    multiclass_roc = make_scorer(multiclass_roc_auc_score,
-                             greater_is_better=True)
-
-    scoring_dict = {'accuracy': 'accuracy',
-                'neg_log_loss': 'neg_log_loss',
-                'roc_auc': multiclass_roc}
-
-    # create lists
-    #scoring_list = ['accuracy', 'roc_auc', 'neg_log_loss']
-
-    classifier_list = [log_reg_clf, rf_clf, gb_clf, xgb_clf]
+    # Standard across datasets
     classifier_name = ['LogReg', 'RandomForest', 'GradientBoosting', 'XGB']
-
     metrics_of_interest = ['fit_time', 'score_time', 'test_accuracy',
-                       'test_neg_log_loss', 'test_roc_auc']
+                           'test_neg_log_loss', 'test_roc_auc',
+                           'train_accuracy', 'train_neg_log_loss',
+                           'train_roc_auc']
+    # Get metrics
+    metric_df = get_metrics(X, y,
+                            classifier_list, classifier_name,
+                            scoring, metrics_of_interest,
+                            n_folds=10)
 
-    metric_dx_df = get_metrics(X, ysub,
-                                classifier_list, classifier_name,
-                                scoring_dict, metrics_of_interest,
-                                n_folds=5)
-
-    metric_dx_df.to_csv(csv_name)
+    metric_df.to_csv(csv_name)
