70a43526b5139eedf469c499228af1baa99a3eaf
Christiana Smith <cj.smith015@gmail.com>
2018-02-02 09:41:59 -0800

    Combine line graph functions into one

--- clustering_functions.py
+++ clustering_functions.py
@@ -111,7 +111,7 @@ def build_piechart(df, data, clf, target, axs,
         ax.pie(cluster_dict.values(), labels=cluster_dict.keys(), radius=(class_len_dict[dx]/total_n)*2, colors=['#ff9000', '#2586bc'])
         ax.set_title(title_dict[dx])
 
-def run_ADHD_Control_k2(df_ADHD, df_control, clf, axs):
+def run_ADHD_Control_k2(df_ADHD, df_control, clf, axs, dataset='TMCQ'):
     y_control = clf.fit_predict(df_control)
     y_adhd = clf.fit_predict(df_ADHD)
 
@@ -132,7 +132,8 @@ def run_ADHD_Control_k2(df_ADHD, df_control, clf, axs):
                     'Cluster 1 Control': {'cluster': cluster1C, 'linestyle': 'dashed', 'marker': '^', 'color':'#7ebbdd', 'mcolor':'#58839b'}
                     }
 
-    tmcq_col_dict = {
+    if dataset == 'TMCQ':
+        col_dict = {
                      'Effortful Control':
                         {'col_labels': ['Impulsivity', 'Inhibition', 'Attentional Focus'],
                          'cols': ['Y1_P_TMCQ_IMPULS', 'Y1_P_TMCQ_INHIBIT', 'Y1_P_TMCQ_ATTFOCUS']},
@@ -145,16 +146,34 @@ def run_ADHD_Control_k2(df_ADHD, df_control, clf, axs):
                      'Misc':
                         {'col_labels': ['Openness', 'Percept', 'LIP'],
                          'cols': ['Y1_P_TMCQ_OPENNESS', 'Y1_P_TMCQ_PERCEPT', 'Y1_P_TMCQ_LIP']}
+                        }
+    elif dataset == 'neuro':
+        col_dict = {
+                     'Speed':
+                        {'col_labels': ['Color Reading', 'Word Naming', 'Trails Condition 2', 'Trails Condition 3'],
+                         'cols': ['Y1_CLWRD_COND1', 'Y1_CLWRD_COND2', 'Y1_TRAILS_COND2', 'Y1_TRAILS_COND3']},
+                     'Inhibition':
+                        {'col_labels': ['Stroop CW Res', 'Trails Res', 'Stop Signal RT'],
+                         'cols': ['CW_RES', 'TR_RES', 'STOP_SSRTAVE_Y1']},
+                     'Arousal':
+                        {'col_labels': ['DPrime Catch', 'DPrime Stim', 'Drift Rate'],
+                         'cols': ['DPRIME1_Y1', 'DPRIME2_Y1', 'V_Y1']},
+                     'Working Memory':
+                        {'col_labels': ['Digit Span-Forward','Digit Span-Backward', 'SSpan-Forward', 'SSpan-Backward'],
+                         'cols': ['Y1_DIGITS_FRWD_RS', 'Y1_DIGITS_BKWD_RS','SSFD_NUMCOMPLETE_Y1','SSBK_NUMCOMPLETE_Y1']},
+                     'Clock':
+                        {'col_labels': ['TAP Clock Std Dev'],
+                         'cols': ['Y1_TAP_SD_TOT_CLOCK']}
                     }
 
-    run_TMCQ_graph(cluster_dict, tmcq_col_dict, axs)
+    run_line_graph(cluster_dict, col_dict, axs)
 
-def run_TMCQ_graph(cluster_dict, tmcq_col_dict, axs):
-    for ax, tmcq_group in zip(axs, tmcq_col_dict.keys()):
-        TMCQ_graph(ax, cluster_dict, tmcq_col_dict[tmcq_group])
-        ax.set_title(tmcq_group)
+def run_line_graph(cluster_dict, col_dict, axs):
+    for ax, group in zip(axs, col_dict.keys()):
+        line_graph(ax, cluster_dict, col_dict[group])
+        ax.set_title(group)
 
-def TMCQ_graph(ax, cluster_dict, col_dict):
+def line_graph(ax, cluster_dict, col_dict):
     ind = range(1, len(col_dict['cols'])+1)
     for label in cluster_dict.keys():
         values = np.mean(cluster_dict[label]['cluster'].loc[:,col_dict['cols']])
@@ -168,64 +187,9 @@ def TMCQ_graph(ax, cluster_dict, col_dict):
         ax.plot(ind, values.values, linestyle=line, linewidth=2.0, color=color, zorder=1)
     ax.set_xticks(ind)
     ax.set_xticklabels(col_dict['col_labels'])
-    ax.set_xlim(0.5, len(col_dict['cols'])+0.5)
-    ax.set_ylabel('TMCQ Score')
+    ax.set_xlim(0.5, len(col_dict['cols'])+1)
+    ax.set_ylabel('Scale Score')
     ax.legend(framealpha=True, borderpad=1.0, facecolor="white")
-
-def run_ADHD_Control_k2_neuro(df_ADHD, df_control, clf, ax, scale=None):
-    y_control = clf.fit_predict(df_control)
-    y_adhd = clf.fit_predict(df_ADHD)
-
-    cluster_df_control = df_control.copy()
-    cluster_df_control['cluster'] = y_control
-    cluster_df_adhd = df_ADHD.copy()
-    cluster_df_adhd['cluster'] = y_adhd
-    if not scale:
-        cluster_df_control.iloc[:,0:-1] = StandardScaler().fit_transform(cluster_df_control.iloc[:,0:-1])
-        cluster_df_adhd.iloc[:,0:-1] = StandardScaler().fit_transform(cluster_df_adhd.iloc[:,0:-1])
-
-    cluster0C = cluster_df_control.loc[cluster_df_control[cluster_df_control['cluster']==0].index,:]
-    cluster1C = cluster_df_control.loc[cluster_df_control[cluster_df_control['cluster']==1].index,:]
-    cluster0A = cluster_df_adhd.loc[cluster_df_adhd[cluster_df_adhd['cluster']==0].index,:]
-    cluster1A = cluster_df_adhd.loc[cluster_df_adhd[cluster_df_adhd['cluster']==1].index,:]
-
-    cluster_dict = {
-                    'Cluster 0 ADHD': {'cluster': cluster0A, 'linestyle': 'solid', 'marker': 'o', 'color':'#ff9000', 'mcolor':'#db7b00'},
-                    'Cluster 1 ADHD': {'cluster': cluster1A, 'linestyle': 'solid', 'marker': 'o', 'color':'#ffbf6d', 'mcolor':'#d19c59'},
-                    'Cluster 0 Control': {'cluster': cluster0C, 'linestyle': 'dashed', 'marker': '^', 'color':'#30a4e5', 'mcolor':'#2586bc'},
-                    'Cluster 1 Control': {'cluster': cluster1C, 'linestyle': 'dashed', 'marker': '^', 'color':'#7ebbdd', 'mcolor':'#58839b'}
-                    }
-
-    neuro_col_dict = {'df_cols': ['STOP_SSRTAVE_Y1', 'DPRIME1_Y1', 'DPRIME2_Y1', 'SSBK_NUMCOMPLETE_Y1',
-                      'SSFD_NUMCOMPLETE_Y1', 'V_Y1', 'Y1_CLWRD_COND1', 'Y1_CLWRD_COND2', 'Y1_DIGITS_BKWD_RS',
-                      'Y1_DIGITS_FRWD_RS', 'Y1_TRAILS_COND2', 'Y1_TRAILS_COND3', 'CW_RES', 'TR_RES', 'Y1_TAP_SD_TOT_CLOCK'],
-                      'col_labels': ['Stop Signal RT','CPT DPrime Catch','CPT Dprime Stim','SSpan Backward Items Attempted',
-                      'SSpan Forward Items Attempted','Drift Rate','Color Word: Color Naming (seconds)',
-                      'Color Word: Word Reading (seconds)','Digit Span Forward Raw Score',
-                      'Digit Span Backward Raw Score','Trails Condition 2 Time (seconds)',
-                      'Trails Condition 3 Time (seconds)','Standardized Residual Score - ColorWord',
-                      'Standardized Residual Score - Trails','Tap SD Total Clock']}
-
-    ind = range(1, len(neuro_col_dict['df_cols'])+1)
-
-    for label in cluster_dict.keys():
-        values = np.mean(cluster_dict[label]['cluster'].loc[:,neuro_col_dict['df_cols']])
-        sem = scs.sem(cluster_dict[label]['cluster'].loc[:,neuro_col_dict['df_cols']], axis=0)
-        line = cluster_dict[label]['linestyle']
-        marker = cluster_dict[label]['marker']
-        color = cluster_dict[label]['color']
-        mcolor = cluster_dict[label]['mcolor']
-        ax.scatter(ind, values.values, label=label, s=125, marker=marker, color=mcolor, zorder=3)
-        ax.errorbar(ind, values.values, yerr=sem, linestyle="None", marker="None", color=color, capsize=6, elinewidth=3, barsabove=False, zorder=2)
-        ax.plot(ind, values.values, linestyle=line, linewidth=3.0, color=color, zorder=1)
-
-    ax.set_xticks(ind)
-    ax.set_xticklabels(neuro_col_dict['col_labels'], fontsize=15)
-    ax.set_xlim(0.5, len(neuro_col_dict['df_cols'])+0.5)
-    for tick in ax.get_xticklabels():
-        tick.set_rotation(90)
-    ax.legend(framealpha=True, borderpad=1.0, facecolor="white", fontsize=15)
-    ax.tick_params('both', labelsize=15)
 
 def wcss_and_silhouette(df, clf, axs, label, max_k=6, standard_scale=False):
     wcss = np.zeros(max_k)
